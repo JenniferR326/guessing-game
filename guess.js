a2e2d1f9755bed714cbf6ff50c9b987dd4c5563e
JenniferR326 <jennarafael@gmail.com>
2021-04-12 12:37:34 -0400

    refactor

--- guess.js
+++ guess.js
@@ -1,22 +1,11 @@
 "use strict";
-
-// To select an element
-// document.querySelector(".message").textContent  //for class use "" and . and for id use "" and //#endregion
-// document.querySelector(".message").textContent = "🎉 Correct!"
-
-// document.querySelector(".number").textContent = 777
-// document.querySelector(".score").textContent = 777
-
-// document.querySelector(".guess").value  //to get the value
-// document.querySelector(".guess").value = 77  //to manipulate the value
-
-//Event is something that happens on the page. Event listener waits for event and reacts
-//First arg to event listener is the type of event. Second arg is a function with event handler functionality
-
 let secretNumber = Math.trunc(Math.random() * 20 + 1);
 
 let score = 20;
 let highScore = 0;
+function displayMessage(msg) {
+  document.querySelector(".message").textContent = msg;
+}
 
 document.querySelector(".number").textContent = "?";
 document.querySelector(".check").addEventListener("click", () => {
@@ -29,7 +18,7 @@ document.querySelector(".check").addEventListener("click", () => {
 
     // When player wins
   } else if (guess === secretNumber) {
-    document.querySelector(".message").textContent = "🎉 You guessed it!";
+    displayMessage("🎉 You guessed it!");
     document.querySelector(".number").textContent = secretNumber;
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
@@ -39,50 +28,24 @@ document.querySelector(".check").addEventListener("click", () => {
       highScore = score;
       document.querySelector(".highscore").textContent = highScore;
     }
-
-    // When guess is too high
   } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector(".message").textContent =
-        guess > secretNumber ? "📈 Too high!" : "📉 Too low!";
+      displayMessage(guess > secretNumber ? "📈 Too high!" : "📉 Too low!");
+
       score--;
       document.querySelector(".score").textContent = score;
     } else {
-      document.querySelector(".message").textContent = "😞 Game over!";
+      displayMessage("😞 Game over!");
       document.querySelector(".score").textContent = 0;
       document.querySelector("body").style.backgroundColor = "#990022";
     }
   }
 });
 
-// else if (guess > secretNumber) {
-// if (score > 1) {
-//   document.querySelector(".message").textContent = "📈 Too high!";
-//   score--;
-//   document.querySelector(".score").textContent = score;
-// } else {
-//   document.querySelector(".message").textContent = "😞 Game over!";
-//   document.querySelector(".score").textContent = 0;
-//   document.querySelector("body").style.backgroundColor = "#990022";
-// }
-// When guess is too low
-// } else if (guess < secretNumber) {
-//   if (score > 1) {
-//     document.querySelector(".message").textContent = "📉 Too low!";
-//     score--;
-//     document.querySelector(".score").textContent = score;
-//   } else {
-//     document.querySelector(".message").textContent = "😞 Game over!";
-//     document.querySelector(".score").textContent = 0;
-//     document.querySelector("body").style.backgroundColor = "#990022"; //style must be a string
-//   }
-// }
-// });
-
 document.querySelector(".again").addEventListener("click", () => {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20 + 1);
-  document.querySelector(".message").textContent = "Start guessing...";
+  displayMessage("Start guessing...");
 
   //displays reset score
   document.querySelector(".score").textContent = score;
