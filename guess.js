0b53d4db0e7b25c54784962f296ae0d5166cc18d
JenniferR326 <jennarafael@gmail.com>
2021-04-12 11:51:52 -0400

    high score functionality working

--- guess.js
+++ guess.js
@@ -16,6 +16,7 @@
 let secretNumber = Math.trunc(Math.random() * 20 + 1);
 
 let score = 20;
+let highScore = 0;
 
 document.querySelector(".number").textContent = "?";
 console.log(secretNumber);
@@ -33,6 +34,12 @@ document.querySelector(".check").addEventListener("click", () => {
     document.querySelector(".number").textContent = secretNumber;
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
+
+    //high score function
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector(".highscore").textContent = highScore;
+    }
 
     // When guess is too high
   } else if (guess > secretNumber) {
@@ -73,5 +80,4 @@ document.querySelector(".again").addEventListener("click", () => {
 
   document.querySelector("body").style.backgroundColor = "#222";
   document.querySelector(".number").style.width = "15rem";
-
 });
